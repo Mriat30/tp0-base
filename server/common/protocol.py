ae2186154fdf0da60ff114457d9dbc0c0849402a
Mateo Riat <mriat@fi.uba.ar>
2026-03-15 18:56:00 -0300

    feat: agrego el metodo read_bet para leer una apuesta del socket la cual construye el objeto apuesta y lo devuelve

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -1,12 +1,40 @@
 import socket
 from common.utils import ActionType
+from model.bet import Bet
 
 class ServerProtocol:
-    _SIZE_ACTION_IN_BYTES = 1
+    _CHAR_SIZE = 1
+    _INT_SIZE = 4
 
     def __init__(self, socket):
-        self.socket = socket
+        self._socket = socket
 
     def read_action(self):
-        action = self.socket.recv(self._SIZE_ACTION_IN_BYTES)
+        action = self._socket.recv(self._CHAR_SIZE)
         return ActionType.from_bytes(action)
+
+    def read_bet(self):
+        agency_raw = self._read_exactly(self._INT_SIZE)
+        agency = str(int.from_bytes(agency_raw, byteorder='big'))
+
+        first_name = self._read_string()
+        last_name = self._read_string()
+        document = self._read_string()
+        birthdate = self._read_string()
+        number = self._read_string()
+
+        return Bet(agency, first_name, last_name, document, birthdate, number)
+
+    def _read_string(self):
+        raw_len = self._socket.recv(self._CHAR_SIZE)
+        if not raw_len: return ""
+        length = int.from_bytes(raw_len, byteorder='big')
+        return self._read_exactly(length).decode('utf-8')
+
+    def _read_exactly(self, n):
+        data = b''
+        while len(data) < n:
+            packet = self._socket.recv(n - len(data))
+            if not packet: raise EOFError("Socket cerrado")
+            data += packet
+        return data
--- server/tests/test_protocol.py
+++ server/tests/test_protocol.py
@@ -1,7 +1,9 @@
 from common.protocol import ServerProtocol
 from common.utils import ActionType
+from model.bet import Bet
 import unittest
 from unittest.mock import MagicMock
+import datetime
 
 class TestProtocol(unittest.TestCase):
     def test_protocol_read_action_success(self):
@@ -23,6 +25,27 @@ class TestProtocol(unittest.TestCase):
         with self.assertRaises(ValueError):
             protocol.read_action()
         
+    def test_protocol_read_bet_success(self):
+            socket = MagicMock()
+            
+            socket.recv.side_effect = [
+                b'\x00\x00\x00\x01', # Agencia (numero 1, como es int se representa con 4 bytes big endian)
+                b'\x05', b'Mateo',    # Nombre (longitud 5, 'Mateo')
+                b'\x05', b'Perez',    # Apellido (longitud 5, 'Perez')
+                b'\x08', b'40000000', # Documento (longitud 8, '40000000')
+                b'\x0a', b'2000-01-01',# Fecha (longitud 10, '2000-01-01')
+                b'\x04', b'7574'      # Numero (longitud 4, '7574')
+            ]
+
+            protocol = ServerProtocol(socket)
+            bet = protocol.read_bet()
+
+            self.assertEqual(bet.agency, 1)
+            self.assertEqual(bet.first_name, 'Mateo')
+            self.assertEqual(bet.last_name, 'Perez')
+            self.assertEqual(bet.document, '40000000')
+            self.assertEqual(bet.birthdate, datetime.date.fromisoformat('2000-01-01'))
+            self.assertEqual(bet.number, 7574)
 
 if __name__ == '__main__':
     unittest.main()
