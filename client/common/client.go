cb452c05f5c37a7ed9335b8ed99611634dccac60
Mateo Riat <mriat@fi.uba.ar>
2026-03-17 10:19:15 -0300

    feat: adapto el cliente para que arme las apuesta y loguee en caso de success o fail

--- client/common/client.go
+++ client/common/client.go
@@ -1,20 +1,19 @@
 package common
 
 import (
-	"bufio"
-	"fmt"
 	"net"
-	"time"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common/model"
+	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common/protocol"
 	"github.com/op/go-logging"
 )
 
 var log = logging.MustGetLogger("log")
 
-// ClientConfig Configuration used by the client
 type ClientConfig struct {
 	ID            string
 	ServerAddress string
@@ -22,15 +21,12 @@ type ClientConfig struct {
 	LoopPeriod    time.Duration
 }
 
-// Client Entity that encapsulates how
 type Client struct {
-	config ClientConfig
-	conn   net.Conn
+	config            ClientConfig
+	conn              net.Conn
 	should_be_running bool
 }
 
-// NewClient Initializes a new client receiving the configuration
-// as a parameter
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config: config,
@@ -38,9 +34,6 @@ func NewClient(config ClientConfig) *Client {
 	return client
 }
 
-// CreateClientSocket Initializes client socket. In case of
-// failure, error is printed in stdout/stderr and exit 1
-// is returned
 func (c *Client) createClientSocket() error {
 	conn, err := net.Dial("tcp", c.config.ServerAddress)
 	if err != nil {
@@ -49,52 +42,59 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
 }
 
-// StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
 	c.should_be_running = true
 	c.handleSigterm()
+
 	for msgID := 1; msgID <= c.config.LoopAmount && c.should_be_running; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
+		err := c.createClientSocket()
 		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
+			time.Sleep(c.config.LoopPeriod)
+			continue
+		}
+
+		bet := model.Bet{
+			Agency:    1,
+			FirstName: "Juan",
+			LastName:  "Perez",
+			Document:  12345678,
+			Birthdate: "1990-01-01",
+			Number:    7574,
+		}
+
+		proto := protocol.NewProtocol(c.conn)
+
+		err = proto.SendBet(bet)
+		if err != nil {
+			log.Errorf("action: send_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			c.conn.Close()
 			return
 		}
 
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
+		log.Infof("action: apuesta_enviada | result: success | dni: %v", bet.Document)
 
-		// Wait a time between sending one message and the next one
+		err = proto.ReadBetRegistered()
+		if err != nil {
+			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			c.conn.Close()
+			return
+		}
+
+		log.Infof("action: apuesta_almacenada | result: success | dni: %v", bet.Document)
+
+		c.conn.Close()
 		time.Sleep(c.config.LoopPeriod)
-
 	}
+
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
 
-// handleSigterm Handles the SIGTERM signal to gracefully shutdown the client
 func (c *Client) handleSigterm() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM)
