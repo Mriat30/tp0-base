e73bf01ad6ae8ee0ea45f707610f4ded79f2f78a
Mateo Riat <mriat@fi.uba.ar>
2026-03-19 18:53:59 -0300

    refactor: ahora el bookmaker recibe un bet provider

--- client/src/domain/book_maker.go
+++ client/src/domain/book_maker.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"io"
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/src/model"
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/src/protocol"
 	"github.com/op/go-logging"
@@ -9,32 +10,41 @@ import (
 var log = logging.MustGetLogger("log")
 
 type Bookmaker struct {
-	id    string
-	proto *protocol.Protocol
-	bet   model.Bet
+	id       string
+	proto    *protocol.Protocol
+	provider model.BetProvider
 }
 
-func NewBookmaker(proto *protocol.Protocol, bet model.Bet, clientID string) *Bookmaker {
+func NewBookmaker(proto *protocol.Protocol, provider model.BetProvider, clientID string) *Bookmaker {
 	return &Bookmaker{
-		proto: proto,
-		bet:   bet,
-		id:    clientID,
+		proto:    proto,
+		provider: provider,
+		id:       clientID,
 	}
 }
 
-func (b *Bookmaker) Register() error {
-	err := b.proto.SendBet(b.bet)
-	if err != nil {
-		log.Errorf("action: send_message | result: fail | client_id: %v | error: %v", b.id, err)
-		return err
+func (b *Bookmaker) RegisterAll(batchSize int) error {
+	for {
+		batch, err := b.provider.NextBatch(batchSize)
+		if err == io.EOF {
+			log.Infof("action: procesamiento_finalizado | result: success | client_id: %v", b.id)
+			break
+		}
+		if err != nil {
+			log.Errorf("action: leer_batch | result: fail | client_id: %v | error: %v", b.id, err)
+			return err
+		}
+		err = b.proto.SendBatch(batch)
+		if err != nil {
+			log.Errorf("action: enviar_batch | result: fail | client_id: %v | error: %v", b.id, err)
+			return err
+		}
+		err = b.proto.ReadBetRegistered()
+		if err != nil {
+			log.Errorf("action: confirmar_batch | result: fail | client_id: %v | error: %v", b.id, err)
+			return err
+		}
+		log.Infof("action: batch_procesado | result: success | client_id: %v | cantidad: %d", b.id, len(batch))
 	}
-	log.Infof("action: apuesta_enviada | result: success | dni: %v", b.bet.Document)
-	err = b.proto.ReadBetRegistered()
-	if err != nil {
-		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v", b.id, err)
-		return err
-	}
-	log.Infof("action: apuesta_almacenada | result: success | dni: %v", b.bet.Document)
-
 	return nil
 }
