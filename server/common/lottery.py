9aadb40bd1665703c7f84c2d4af9305248c3d075
Mateo Riat <mriat@fi.uba.ar>
2026-03-21 15:21:40 -0300

    fix: separo el lock de storage del lock de condvar

--- server/common/lottery.py
+++ server/common/lottery.py
@@ -7,26 +7,30 @@ class Lottery:
         self._agencies_done = set()
         self._total_agencies = total_agencies
         self._logger = logger
-        self._lock = Lock()
-        self._lottery_done = Condition(self._lock)
+        self._storage_lock = Lock()   
+        self._condition = Condition()
         self._winners = None
 
     def store_bets(self, bets):
-        with self._lock:
+        with self._storage_lock:
             store_bets(bets)
 
     def notify_done(self, agency_id):
-        with self._lottery_done:
+        with self._condition:
             self._agencies_done.add(agency_id)
             if len(self._agencies_done) == self._total_agencies:
-                self._winners = list(load_bets())
+                self._winners = self._load_bets_safe()
                 self._logger.info("action: sorteo | result: success")
-                self._lottery_done.notify_all()
+                self._condition.notify_all()
             else:
-                self._lottery_done.wait_for(lambda: self._winners is not None)
+                self._condition.wait_for(lambda: self._winners is not None)
 
         return [
             LotteryWinner(bet.document)
             for bet in self._winners
             if bet.agency == agency_id and has_won(bet)
         ]
+
+    def _load_bets_safe(self):
+        with self._storage_lock:
+            return list(load_bets())
