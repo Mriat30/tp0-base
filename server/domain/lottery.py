37cfa984cdc19a7212556675c52e9e812c17bfb1
Mateo Riat <mriat@fi.uba.ar>
2026-03-26 02:32:45 -0300

    feat: implemento una lottery con Condition (Monitor) simplemente para no usar un lock explicito con la ventaja de que no manejo el lock

--- server/domain/lottery.py
+++ server/domain/lottery.py
@@ -1,29 +1,32 @@
-from threading import Event, Lock
+from threading import Condition
 from model.bet import has_won
 from model.lottery_winner import LotteryWinner
 
 class Lottery:
     def __init__(self, total_agencies, storage, logger):
-        self._agencies_done = set()
         self._total_agencies = total_agencies
         self._logger = logger
         self._storage = storage
-        self._lottery_ready = Event()
-        self._lock = Lock()
+        self._condition = Condition()
+        self._agencies_done = set()
         self._winners = {}
+        self._lottery_done = False
 
     def notify_done(self, agency_id):
-        with self._lock:
+        with self._condition:
             self._agencies_done.add(agency_id)
-            if len(self._agencies_done) == self._total_agencies:
-                self._run_lottery()
+            should_run = len(self._agencies_done) == self._total_agencies
+
+        if should_run:
+            self._run_lottery()
+            with self._condition:
+                self._lottery_done = True
+                self._condition.notify_all()
 
     def get_winners(self, agency_id):
-        self._lottery_ready.wait()
-        return [
-            LotteryWinner(doc)
-            for doc in self._winners.get(agency_id, [])
-        ]
+        with self._condition:
+            self._condition.wait_for(lambda: self._lottery_done)
+            return [LotteryWinner(doc) for doc in self._winners.get(agency_id, [])]
 
     def _run_lottery(self):
         all_bets = self._storage.load()
@@ -31,4 +34,3 @@ class Lottery:
             if has_won(bet):
                 self._winners.setdefault(bet.agency, []).append(bet.document)
         self._logger.info("action: sorteo | result: success")
-        self._lottery_ready.set()
