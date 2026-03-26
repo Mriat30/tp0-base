c9d01c87521fd4f366f48c3e3c17143b39084968
Mateo Riat <mriat@fi.uba.ar>
2026-03-26 00:59:40 -0300

    feat: agrego el healthcheck al ej2

--- mi-generador.py
+++ mi-generador.py
@@ -25,6 +25,14 @@ def main():
         "      - PYTHONUNBUFFERED=1",
         "    networks:",
         "      - testing_net",
+        "    healthcheck:",
+        "      test:",
+        "        - CMD",
+        "        - sh",
+        "        - /healthcheck.sh",
+        "      interval: 2s",
+        "      timeout: 5s",
+        "      retries: 5",
         ""
     ]
 
@@ -40,7 +48,8 @@ def main():
         content.append(f"    networks:")
         content.append(f"      - testing_net")
         content.append(f"    depends_on:")
-        content.append(f"      - server")
+        content.append(f"      server:")
+        content.append(f"        condition: service_healthy")
         content.append("")
 
     content.extend([
