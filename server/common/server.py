e3f319973eeaf364f2b162297f1c0f8767e6783f
Mateo Riat <mriat@fi.uba.ar>
2026-03-26 01:54:09 -0300

    feat: mejoro el manejo de clear para cuando el socket ya esta cerrado

--- server/common/server.py
+++ server/common/server.py
@@ -91,7 +91,10 @@ class Server:
         client = self.client
         if client is None:
             return
-        else:
+        try:
             client.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass
+        finally:
             client.close()
             self.client = None
